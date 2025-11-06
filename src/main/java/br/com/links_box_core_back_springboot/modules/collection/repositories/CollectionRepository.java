14505c631e53be8812d3b65a44d850a3659d0e07
leandroltfx <leandroltfbr@hotmail.com>
2025-11-06 08:03:51 -0300

    remove função não utilizada

--- src/main/java/br/com/links_box_core_back_springboot/modules/collection/repositories/CollectionRepository.java
+++ src/main/java/br/com/links_box_core_back_springboot/modules/collection/repositories/CollectionRepository.java
@@ -6,14 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CollectionRepository extends JpaRepository<CollectionEntity, UUID> {
 
-    List<CollectionEntity> findAllByUserId(UUID userId);
-    
     Page<CollectionEntity> findAllByUserId(UUID userId, Pageable pageable);
 
 }
